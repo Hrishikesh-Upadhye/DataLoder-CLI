a12e5cb53d78d9600a9c0182fa8b7bc9fc79c8da
Hrishikesh Upadhye <hsupadhye@gmail.com>
2017-10-07 23:58:09 +0530

    Update process1.bat

--- Bin/process1.bat
+++ Bin/process1.bat
@@ -1,10 +1,9 @@
 echo "starting"
 cls
-F:
-cd "F:\Softwares\Salesforce.com\bin"
-CALL process.bat "F:\Softwares\DataLoader CLI\Config" "contactInsert"
+cd "C:\Program Files (x86)\salesforce.com\Data Loader\bin"
+CALL process.bat "C:\DataLoader CLI\Config" "contactInsert"
 
-CALL WinRAR a -r -agYYYY-MM-DD -df "F:\Softwares\DataLoader CLI\Archived\Backup.rar" "F:\Softwares\DataLoader CLI\Log\*.*"
+CALL WinRAR a -r -agYYYY-MM-DD -df "C:\DataLoader CLI\Archived\Backup.rar" "C:\Softwares\DataLoader CLI\Log\*.*"
 
 
 pause
